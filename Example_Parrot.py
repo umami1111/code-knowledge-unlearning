aba6dbcfda50ebd34c1d95b6187822cb444e13c1
Abdullah <abdullah.khan4620@gmail.com>
2024-04-10 05:24:18 -0400

    deprecated argument

--- Example_Parrot.py
+++ Example_Parrot.py
@@ -17,11 +17,11 @@ def codeparrot(prompts, model_name, output_paths, num_prompts_per_gen=1):
         output_paths = [output_paths]
     assert len(prompts) == len(output_paths)
     pipe = pipeline("text-generation", model=model_name, pad_token_id=50256, device=0, batch_size=num_prompts_per_gen)
-    
+
     if num_prompts_per_gen > 1 and pipe.model.__class__.__name__.startswith("GPT2"):
         # IMPORTANT: Change the configuration of tokenizer to make batching work for GPT2
         # cf.
-        #   https://github.com/huggingface/transformers/issues/21202 
+        #   https://github.com/huggingface/transformers/issues/21202
         # Since gpt2 doesn't have a pad_token
         if not pipe.tokenizer.special_tokens_map.get("pad_token"):
             pad_token = {"pad_token":"<|endoftext|>"}
@@ -35,7 +35,7 @@ def codeparrot(prompts, model_name, output_paths, num_prompts_per_gen=1):
         pipe.tokenizer.padding_side = "left" #For BERT like models use "right"
 
     gen_start_time = datetime.now()
-    outputs = pipe(prompts, num_return_sequences=NUM_PER_PROMPT, max_length=256)
+    outputs = pipe(prompts, num_return_sequences=NUM_PER_PROMPT, max_new_tokens=256)
     print(f"Generated {num_prompts_per_gen} prompts * {NUM_PER_PROMPT} files: {(datetime.now() - gen_start_time).total_seconds()} [sec]")
     for i, output_path in enumerate(output_paths):
         for j in range(NUM_PER_PROMPT):
